b397eed6d83617825be2fc06aa58e32eb2f25412
Marah-Jaber <marahjaber@hotmail.com>
2017-04-15 09:13:15 +0300

    change

--- JHotDraw 7.0.6/src_groupA/groupA/figure/DefaultFigureFactory.java
+++ JHotDraw 7.0.6/src_groupA/groupA/figure/DefaultFigureFactory.java
@@ -26,19 +26,6 @@ public class DefaultFigureFactory implements FigureFactory {
 		return new RectangleFigure();
 	}
 
-/*	*//**
-	 * @see org.simpleeditor.figure.FigureFactory#getDecoratedFigure(org.simpleeditor.figure.FigureType)
-	 *//*
-	@Override
-	public Figure getDecoratedFigure(FigureType type) {
-		// We implemented only line figure as an example of the decorator design
-		// pattern.
-		LineFigure decoratedArrow = (LineFigure) new LineFigure();
-		decoratedArrow.set(AttributeKeys.START_DECORATION,
-				new org.jhotdraw.draw.decoration.ArrowTip(0.35, 12, 11.3));
-		// decoratedArrow.set(AttributeKeys.END_DECORATION, new
-		// org.jhotdraw.draw.decoration.ArrowTip(0.35, 12, 11.3));
-		return decoratedArrow;
-	}*/
+
 
 }
